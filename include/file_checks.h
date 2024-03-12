27bc90c2abf275a18c0060d043de81bbb8f0f1fb
He1KoR <helkor.rightway@gmail.com>
2024-03-12 19:22:54 +0300

    Added ifstream file checks

--- include/file_checks.h
+++ include/file_checks.h
@@ -4,6 +4,11 @@
 bool checkOpenedError(std::fstream& file);
 bool checkNotOpenedError(std::fstream& file);
 bool checkFileIssues(std::fstream& file);
+
+bool checkOpenedError(std::ifstream& file);
+bool checkNotOpenedError(std::ifstream& file);
+bool checkFileIssues(std::ifstream& file);
+
 bool checkFileExtensionError(const char file_path[], const char file_extension[]);
 bool checkFileExistError(const char file_path[]);
 bool checkFileNotExistError(const char file_path[]);
