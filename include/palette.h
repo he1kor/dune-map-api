76d9d808c63a0466229b70e3185e9ee8a3863d72
He1KoR <helkor.rightway@gmail.com>
2024-03-13 07:58:32 +0300

    FindMaterial added to palette and pick by material object

--- include/palette.h
+++ include/palette.h
@@ -11,8 +11,10 @@ class Palette{
         Palette(Palette&& palette);
         Palette(Palette& palette);
         Palette& operator=(Palette&& palette);
+        static uint16_t pick(Material material);
         uint16_t pick(std::string material_name);
         const Material& operator[](std::string material_name);
+        Material findMaterial(uint16_t id);
         void addMaterial(std::string material_name, const Material& material);
         void removeMaterial(std::string material_name);
     private:
--- src/tileset/palette/palette.cpp
+++ src/tileset/palette/palette.cpp
@@ -1,6 +1,8 @@
 #include "palette.h"
 #include <random>
 #include <time.h>
+#include <stdexcept>
+
 
 Palette::Palette(){
     srand(time(0));
@@ -31,12 +33,23 @@ const Material& Palette::operator[](std::string material_name){
     return materials.at(material_name);
 }
 
+Material Palette::findMaterial(uint16_t id)
+{
+    for (auto &material_pair : materials)
+    {
+        if (material_pair.second.containsTile(id))
+            return material_pair.second;
+    }
+    return Material(id);
+}
+
 void Palette::removeMaterial(std::string material_name){
     materials.erase(materials.find(material_name));
 }
-
-uint16_t Palette::pick(std::string material_name){
-    Material& material = materials.at(material_name);
+uint16_t Palette::pick(Material material){
     int ranged_rand = std::rand() % material.size();
     return material[ranged_rand];
+}
+uint16_t Palette::pick(std::string material_name){
+    return pick((*this)[material_name]);
 }
