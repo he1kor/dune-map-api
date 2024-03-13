d2a101f709c931d3d13c0e621e085f933b7f2489
He1KoR <helkor.rightway@gmail.com>
2024-03-13 07:57:35 +0300

    Material: addTile, uint16_t constr, operator ==, const size, containstTile()

--- include/material.h
+++ include/material.h
@@ -5,9 +5,13 @@
 class Material{
     public:
         Material();
+        Material(uint16_t tile);
         Material(int x, int y, int width, int height);
+        bool containsTile(uint16_t tile) const;
         const uint16_t& operator[](int i);
-        int size();
+        void addTile(uint16_t tile);
+        bool operator==(const Material& other) const;
+        int size() const;
     private:
         std::vector<uint16_t> tiles;
 };
--- src/tileset/palette/material.cpp
+++ src/tileset/palette/material.cpp
@@ -3,6 +3,8 @@
 
 Material::Material(){}
 
+Material::Material(uint16_t tile) : tiles{tile}{}
+
 Material::Material(int x, int y, int width, int height){
     for (int r = y; r < y + height; r++){
         for (int c = x; c < x + width; c++){
@@ -10,10 +12,37 @@ Material::Material(int x, int y, int width, int height){
         }
     }
 }
-const uint16_t& Material::operator[](int i){
+bool Material::containsTile(uint16_t unknown_tile) const{
+    for (auto &existing_tile : tiles)
+    {
+        if (existing_tile == unknown_tile)
+            return true;
+    }
+    return false;
+}
+
+const uint16_t &Material::operator[](int i)
+{
     return tiles.at(i);
 }
 
-int Material::size(){
+void Material::addTile(uint16_t tile){
+    tiles.push_back(tile);
+}
+
+bool Material::operator==(const Material &other) const{
+    if (tiles.size() != other.tiles.size())
+        return false;
+
+    std::vector<uint16_t> sortedThis = tiles;
+    std::vector<uint16_t> sortedOther = other.tiles;
+
+    std::sort(sortedThis.begin(), sortedThis.end());
+    std::sort(sortedOther.begin(), sortedOther.end());
+
+    return sortedThis == sortedOther;
+}
+
+int Material::size() const{
     return tiles.size();
 }
