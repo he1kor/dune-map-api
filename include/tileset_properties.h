ee334f4740bc9bbf1353e84005769b6be84803da
He1KoR <helkor.rightway@gmail.com>
2024-04-08 22:29:57 +0300

    Added BlockPlacer to tileset properties

--- include/tileset_properties.h
+++ include/tileset_properties.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "palette.h"
 #include "compatible_checker.h"
+#include "block_placer.h"
 
 struct TilesetProperties
 {
     Palette palette;
     CompatibleChecker compatible_checker;
+    BlockPlacer blockPlacer;
 };
