cb456cf7fc66ddba7018148b2c26f9b8f27d2ffc
He1KoR <helkor.rightway@gmail.com>
2024-04-09 17:22:42 +0300

    Block definition moved from block_placer.cpp to block_set.cpp

--- src/tileset/blocks/block_set.cpp
+++ src/tileset/blocks/block_set.cpp
@@ -1,6 +1,49 @@
 #include "block_set.h"
+#include "global.h"
 #include <stdexcept>
 #include <format>
+
+Block::Block(int x, int y, int width, int height) : width{width}, height{height}{
+    tiles = std::vector<std::vector<uint16_t>>(height, std::vector<uint16_t>());
+    for (int r = 0; r < height; r++){
+        left.push_back((r+y) * d2kmapapi::tileset_width + x);
+        right.push_back((r+y) * d2kmapapi::tileset_width + x + width-1);
+        for (int c = 0; c < width; c++){
+            tiles[r].push_back((r+y) * d2kmapapi::tileset_width + (c+x));
+        }
+    }
+}
+
+Block::Block(const Block &block) : width(block.width), height(block.height){
+    left = block.left;
+    right = block.right;
+    tiles = block.tiles;
+}
+
+std::vector<std::vector<uint16_t>> Block::getMatrix() const{
+    return tiles;
+}
+
+std::vector<uint16_t> Block::getTopTiles() const{
+    return tiles[0];
+}
+std::vector<uint16_t> Block::getLeftTiles() const{
+    return left;
+}
+std::vector<uint16_t> Block::getRightTiles() const{
+    return right;
+}
+std::vector<uint16_t> Block::getBottomTiles() const{
+    return tiles[height-1];
+}
+
+int Block::getWidth() const{
+    return width;
+}
+int Block::getHeight() const{
+    return height;
+}
+
 
 BlockSet::BlockSet(const std::map<std::string, std::vector<Block>> &block_groups) : block_groups{block_groups}{}
 
