7c4a32b3b50b2d3fddb2b544902aec92004da859
He1KoR <helkor.rightway@gmail.com>
2024-04-12 20:17:56 +0300

    Added doc comments

--- include/util.h
+++ include/util.h
@@ -1,14 +1,33 @@
+/**
+ * \file
+ * \brief Contains all util functions in d2kmapapi namespace which used in the library.
+ * 
+*/
 #pragma once
 #include <vector>
 
 namespace d2kmapapi{
+    /**
+     * \brief Used for pointing directiong from a tile.
+    */
     enum Direction {
         UP = 0,
         LEFT = 1,
         DOWN = 2,
         RIGHT = 3
     };
+    /**
+     * \brief Returns reversed given direction.
+     * \param direction Direction to be reversed.
+     * \return Reversed direction.
+    */
     Direction reverse(Direction direction);
+    /**
+     * \brief returns True if subarray is contigous array contained in array, else returns False.
+     * \param array Array which possibly contains subarray
+     * \param subarray Array which is possibly subarray of previous array
+     * \return True if subarray is actually subarray of array, else False
+    */
     template <typename T> bool isSubarray(std::vector<T> array, std::vector<T> subarray){
         for (int i = 0; i <= array.size()-subarray.size(); i++) {
             int j;
