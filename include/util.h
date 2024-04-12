618e93f49e794aba61b5d01c2e9c3b16db638c59
He1KoR <helkor.rightway@gmail.com>
2024-04-12 03:27:21 +0300

    isSabarray definition moved to .h

--- include/util.h
+++ include/util.h
@@ -9,5 +9,18 @@ namespace d2kmapapi{
         RIGHT = 3
     };
     Direction reverse(Direction direction);
-    template<typename T> bool isSubarray(std::vector<T> array, std::vector<T> subarray);
+    template <typename T> bool isSubarray(std::vector<T> array, std::vector<T> subarray){
+        for (int i = 0; i <= array.size()-subarray.size(); i++) {
+            int j;
+            for (j = 0; j < subarray.size(); j++) {
+                if (array[i+j] != subarray[j]) {
+                    break;
+                }
+            }
+            if (j == subarray.size()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
