223a547d08c048926725a6afb14aa0ff4bb98fc0
He1KoR <helkor.rightway@gmail.com>
2024-04-15 02:55:16 +0300

    Global doc added

--- include/global.h
+++ include/global.h
@@ -1,6 +1,19 @@
+/**
+ * \file
+ * \brief Contains all constants for the library.
+*/
+
 #pragma once
 
 namespace d2kmapapi{
+    
+    /**
+     * \brief Determines max height or max width of the map.
+    */
     extern const int max_map_size;
+
+    /**
+     * \brief Determines what width (size) to consider for tileset line. Required for processing Block and Material search in tileset, when they're placed at different tileset "lines".
+    */
     extern const int tileset_width;
 }
