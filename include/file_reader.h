ed4df4f7b72cd1937081693d81b7a9f904c5f788
He1KoR <helkor.rightway@gmail.com>
2024-04-15 02:51:43 +0300

    file_reader.h docs added

--- include/file_reader.h
+++ include/file_reader.h
@@ -5,8 +5,19 @@
 */
 class FileReader{
     public:
+        /**
+         * \brief Returns whether the file is opened.
+         * \return True, if file is opened, else False
+        */
         bool fileOpened();
+        /**
+         * \brief Opens file with provided file_path.
+         * \param path to the file from this executive file.
+        */
         void virtual open(const char file_path[]);
+        /**
+         * \brief Closes the file.
+        */
         void virtual close();
         const char* FILE_EXTENSION;
         std::fstream file;
