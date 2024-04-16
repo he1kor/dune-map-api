9c9f9066d6b2ee4181acc200558e3c5e35d238b4
He1KoR <helkor.rightway@gmail.com>
2024-04-16 18:29:27 +0300

    direction_line lines renamed to tiles

--- src/tileset/blocks/directional_line.cpp
+++ src/tileset/blocks/directional_line.cpp
@@ -1,7 +1,7 @@
 #include "directional_line.h"
 #include "util.h"
 
-DirectionalLine::DirectionalLine(const std::vector<uint16_t>& lines, d2kmapapi::Direction normal_direction) : lines{lines}, normal_direction{normal_direction}{}
+DirectionalLine::DirectionalLine(const std::vector<uint16_t>& tiles, d2kmapapi::Direction normal_direction) : tiles{tiles}, normal_direction{normal_direction}{}
 
 void DirectionalLine::reverse(){
     normal_direction = d2kmapapi::reverse(normal_direction);
