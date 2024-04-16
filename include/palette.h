1cf471725f3e749d23d278d9e8464582e43dc1d7
He1KoR <helkor.rightway@gmail.com>
2024-04-16 17:47:52 +0300

    String as param became sconst

--- include/palette.h
+++ include/palette.h
@@ -4,21 +4,25 @@
 #include <cstdint>
 #include <string>
 /*!
-	\brief Class-wrapper for all materials. 
+	\brief Class-wrapper for all materials. Materials are mapped by their names. Materials are used for random picking tile ID.
 */
 class Palette{
     public:
-        Palette(std::map<std::string, Material>& materials);
+        /**
+         * \brief Constructor with initial material map.
+         * \param materials Map string to Material for copying it to material field.
+        */
+        Palette(const std::map<std::string, Material>& materials);
         Palette();
         Palette(Palette&& palette);
-        Palette(Palette& palette);
+        Palette(const Palette& palette);
         Palette& operator=(Palette&& palette);
-        static uint16_t pick(Material material);
-        uint16_t pick(std::string material_name);
-        const Material& operator[](std::string material_name);
+        static uint16_t pick(const Material& material);
+        uint16_t pick(const std::string& material_name);
+        const Material& operator[](const std::string& material_name);
         Material findMaterial(uint16_t id);
-        void addMaterial(std::string material_name, const Material& material);
-        void removeMaterial(std::string material_name);
+        void addMaterial(const std::string& material_name, const Material& material);
+        void removeMaterial(const std::string& material_name);
     private:
         std::map<std::string, Material> materials;
 };
