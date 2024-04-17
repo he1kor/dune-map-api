2166195bf45c94de532f5105ab8581415dfc9517
He1KoR <helkor.rightway@gmail.com>
2024-04-17 07:45:53 +0300

    Added test for correct compatible blocks find and time of it

--- test/first_test.cpp
+++ test/first_test.cpp
@@ -6,6 +6,7 @@
 #include "util.h"
 #include <tileset_json_i.h>
 #include <iostream>
+#include <chrono>
 
 void printMap(Map& map){
     std::cout << "\n";
@@ -23,12 +24,12 @@ int main() {
     map_bin_file.open("temp.map");
     Map map = map_bin_file.load();
     Painter painter(&map, &tilesetProperties.palette);
-    uint16_t tile = tilesetProperties.palette.pick("rock");
-    tilesetProperties.compatible_checker.areCompatible(tile, d2kmapapi::Direction::RIGHT, tile);
-    Vertical vert = Vertical::fromLeft(0, 0);
     painter.fill(40, 23, "sand", true);
     painter.fill(0, 0, "rock", true);
     painter.fill(40, 0, "rock", true);
+    uint16_t tile = tilesetProperties.palette.pick("rock");
+    tilesetProperties.compatible_checker.areCompatible(tile, d2kmapapi::Direction::RIGHT, tile);
+    Vertical vert = Vertical::fromLeft(0, 0);
     auto groups = tilesetProperties.block_set.getGroups();
     for (auto group : groups){
         std::cout << group << ":\n";
@@ -44,8 +45,38 @@ int main() {
         }
         std::cout << "\n------------------------\n";
     }
+    std::cout << "\n<<------------------------>>\n";
+    auto block = tilesetProperties.block_set["rock_cliffs"][1];
+    auto m = block.getMatrix();
+    auto t = block.getRightTiles();
+    for (auto t1 : t){
+        std::cout << t1 << "\t";
+    }
+    std::cout << "\n\n";
+    for (auto v : m){
+        for (uint16_t t : v){
+            std::cout << t << "\t";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    auto blocks = tilesetProperties.block_set.compatibleBlocks(DirectionalLine(tilesetProperties.block_set["rock_cliffs"][1].getRightTiles(), d2kmapapi::Direction::RIGHT), "rock_cliffs");
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    
+    for (auto block : blocks){
+        auto m = block.getMatrix();
+            for (auto v : m){
+                for (uint16_t t : v){
+                    std::cout << t << "\t";
+                }
+                std::cout << "\n";
+            }
+            std::cout << "\n";
+    }
     map_bin_file.save(map);
     map_bin_file.close();
-
+    std::cout << "passed in " << duration.count() << " microsecs";
     return 0;
 }
