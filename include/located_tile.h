d252a4303c1695203b6c707e32e61d36100828c1
he1kor <helkor.rightway@gmail.com>
2024-05-24 21:27:25 +0300

    Added < operator for located_tile to fix compile error

--- include/located_tile.h
+++ include/located_tile.h
@@ -4,6 +4,7 @@
 
 struct LocatedTile : Tile{
     LocatedTile(Tile tile, int x, int y);
+    bool operator<(const LocatedTile& lt) const;
     int x;
     int y;
 };
--- src/structures/located_tile.cpp
+++ src/structures/located_tile.cpp
@@ -4,3 +4,11 @@ LocatedTile::LocatedTile(Tile tile, int x, int y) : x{x}, y{y}{
     tileID = tile.tileID;
     entityID = tile.entityID;
 }
+
+bool LocatedTile::operator<(const LocatedTile &lt) const{
+    return 
+        x < lt.x || 
+        (x == lt.x && y < lt.y) ||
+        (x == lt.x && y == lt.y && tileID < lt.tileID) ||
+        (x == lt.x && y == lt.y && tileID == lt.tileID && entityID < lt.entityID);
+}
