c6ca5efd0b9e4d8cc5e7594a2183734f1b5015b0
he1kor <helkor.rightway@gmail.com>
2024-05-24 21:39:47 +0300

    Added history stack support to block placer

--- include/block_placer.h
+++ include/block_placer.h
@@ -6,6 +6,7 @@
 #include "map.h"
 #include "edge.h"
 #include "compatible_checker.h"
+#include "history_stack.h"
 /*!
 	\brief Class allows to work with BlockSet on certain map. For that, it contains BlockSet* and Map*.
 */
@@ -23,6 +24,8 @@ class BlockPlacer{
         */
         BlockPlacer(BlockSet* block_set);
 
+        void setHistoryStack(HistoryStack* history_stack);
+
         /**
          * \brief Places provided block on the map. Top-left corner of the block is located at [x, y].
          * \param x x position of top-left block corner on the map
@@ -38,5 +41,6 @@ class BlockPlacer{
     private:
         CompatibleChecker* compatible_checker;
         BlockSet* block_set;
+        HistoryStack* history_stack;
         Map* map;
 };
--- src/tileset/blocks/block_placer.cpp
+++ src/tileset/blocks/block_placer.cpp
@@ -8,13 +8,30 @@ BlockPlacer::BlockPlacer(){}
 
 BlockPlacer::BlockPlacer(BlockSet *block_set) : block_set{block_set}{}
 
+void BlockPlacer::setHistoryStack(HistoryStack *history_stack){
+    this->history_stack = history_stack;
+}
+
 void BlockPlacer::place(int x, int y, const Block &block){
     std::vector<std::vector<uint16_t>> tiles = block.getMatrix();
+    if (!history_stack){
+        for (int r = 0; r < tiles.size(); r++){
+            for (int c = 0; c < tiles.size(); c++){
+                map[y+r][x+c] = tiles[r][c];
+            }
+        }
+        return;
+    }
     for (int r = 0; r < tiles.size(); r++){
         for (int c = 0; c < tiles.size(); c++){
             map[y+r][x+c] = tiles[r][c];
+            history_stack->trackChange({
+                {tiles[r][c], (*map)[r][c].entityID},
+                x+c, y+r
+            });
         }
     }
+    history_stack->commit();
 }
 
 bool BlockPlacer::fit(const Edge &edge) const
