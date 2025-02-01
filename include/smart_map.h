90abd31e2ce052bfb39e32d46840b4eee2a10902
he1kor <helkor.rightway@gmail.com>
2025-02-01 21:42:47 +0300

    feat: add commit for smartmap to commit its history stack, fix uninitialized history_stack ptr

--- include/smart_map.h
+++ include/smart_map.h
@@ -9,15 +9,21 @@ class SmartMap : public Map{
 public: 
     SmartMap(const SmartMap& map);
     SmartMap(uint16_t width, uint16_t height);
+
     static SmartMap fromMap(const Map& map);
+
     void setHistoryStack(HistoryStack* history_stack);
+
+    void apply(Tile tile, int x, int y);
+    bool commit();
+
     DirectionalLine getLineFacingEdge(const Edge& edge, const d2kmapapi::Direction& facingDirection);
 
     Tile SmartMap::getTileByCoords(std::pair<int, int> coords);
     std::vector<Tile> SmartMap::getTilesByCoords(std::vector<std::pair<int, int>> coords);
-    
+
     uint16_t getTileIDByCoords(std::pair<int, int> coords);
     std::vector<uint16_t> getTileIDsByCoords(std::vector<std::pair<int, int>> coords);
 private:
-    HistoryStack* history_stack;
+    HistoryStack* history_stack = nullptr;
 };
--- src/structures/smart_map.cpp
+++ src/structures/smart_map.cpp
@@ -1,4 +1,5 @@
 #include "smart_map.h"
+#include <stdexcept>
 
 SmartMap::SmartMap(const SmartMap &map) : Map(map){}
 
@@ -12,14 +13,20 @@ void SmartMap::setHistoryStack(HistoryStack *history_stack){
     this->history_stack = history_stack;
 }
 
-DirectionalLine SmartMap::getLineFacingEdge(const Edge &edge, const d2kmapapi::Direction &facingDirection){
+bool SmartMap::commit(){
+    if (!history_stack)
+        throw std::runtime_error("History stack is not initialized.");
+    return this->history_stack->commit();
+}
+
+DirectionalLine SmartMap::getLineFacingEdge(const Edge &edge, const d2kmapapi::Direction &facingDirection)
+{
     std::vector<uint16_t> edge_tiles;
     for (auto [x, y] : facingDirection == d2kmapapi::Direction::RIGHT || facingDirection == d2kmapapi::Direction::DOWN ? edge.onAfter() : edge.onBefore()){
         edge_tiles.push_back((*this)[y][x].tileID);
     }
     return DirectionalLine(edge_tiles, facingDirection);
 }
-
 
 Tile SmartMap::getTileByCoords(std::pair<int, int> coords){
     return (*this)[coords.first][coords.second];
