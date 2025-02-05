4f1a4d34bf10f8f63f08a3b37975c78842887316
he1kor <helkor.rightway@gmail.com>
2025-02-05 12:17:19 +0300

    Feat: add bunch of useful methods for SmartMap

--- include/smart_map.h
+++ include/smart_map.h
@@ -5,7 +5,7 @@
 #include "directional_line.h"
 #include "edge.h"
 
-class SmartMap : protected Map{
+class SmartMap : public Map{
 public: 
     SmartMap(const SmartMap& map);
     SmartMap(uint16_t width, uint16_t height);
@@ -15,11 +15,24 @@ public:
     void setHistoryStack(HistoryStack* history_stack);
     HistoryStack* getHistoryStack();
 
-    //void apply(Tile tile, int x, int y);
     bool commit();
     void commitTile(int x, int y, Tile tile);
+    void commitTileID(int x, int y, uint16_t tile_id);
+    void commitEntityID(int x, int y, uint16_t entity_id);
+    void commitTile(LocatedTile located_tile);
+
     void addTile(int x, int y, Tile tile);
+    void addTileID(int x, int y, uint16_t tile_id);
+    void addEntityID(int x, int y, uint16_t entity_id);
+    void addTile(LocatedTile located_tile);
+
     void setUntrackedTile(int x, int y, Tile tile);
+    void setUntrackedTileID(int x, int y, uint16_t tile_id);
+    void setUntrackedEntityID(int x, int y, uint16_t entity_id);
+    void setUntrackedTile(LocatedTile located_tile);
+    Tile getTile(int x, int y);
+    uint16_t getTileID(int x, int y);
+    uint16_t getEntityD(int x, int y);
 
     DirectionalLine getLineFacingEdge(const Edge& edge, const d2kmapapi::Direction& facingDirection);
 
@@ -31,5 +44,5 @@ public:
 private:
     SmartMap(const Map& map);
     HistoryStack* history_stack = nullptr;
-
+    using Map::operator[];
 };
--- src/structures/smart_map.cpp
+++ src/structures/smart_map.cpp
@@ -25,11 +25,35 @@ HistoryStack* SmartMap::getHistoryStack(){
 
 bool SmartMap::commit(){
     if (!history_stack)
-        throw std::runtime_error("History stack is not initialized.");
+        return false;
     return this->history_stack->commit();
 }
 void SmartMap::setUntrackedTile(int x, int y, Tile tile){
     matrix[y][x] = tile;
+}
+
+void SmartMap::setUntrackedTileID(int x, int y, uint16_t tile_id){
+    matrix[y][x].tileID = tile_id;
+}
+
+void SmartMap::setUntrackedEntityID(int x, int y, uint16_t entity_id){
+    matrix[y][x].entityID = entity_id;
+}
+
+void SmartMap::setUntrackedTile(LocatedTile located_tile){
+    matrix[located_tile.y][located_tile.x] = located_tile;
+}
+
+Tile SmartMap::getTile(int x, int y){
+    return matrix[y][x];
+}
+
+uint16_t SmartMap::getTileID(int x, int y){
+    return matrix[y][x].tileID;
+}
+
+uint16_t SmartMap::getEntityD(int x, int y){
+    return matrix[y][x].entityID;
 }
 
 void SmartMap::commitTile(int x, int y, Tile tile){
@@ -39,12 +63,79 @@ void SmartMap::commitTile(int x, int y, Tile tile){
     }
     history_stack->quickCommit({tile, x, y});
 }
+void SmartMap::commitTileID(int x, int y, uint16_t tile_id){
+    if(!history_stack){
+        setUntrackedTileID(x, y, tile_id);
+        return;
+    }
+    history_stack->quickCommit(LocatedTile{
+        tile_id,
+        matrix[y][x].entityID,
+        x,
+        y
+    });
+}
+void SmartMap::commitEntityID(int x, int y, uint16_t entity_id){
+    if(!history_stack){
+        setUntrackedEntityID(x, y, entity_id);
+        return;
+    }
+    history_stack->quickCommit(LocatedTile(
+        matrix[y][x].tileID,
+        entity_id,
+        x,
+        y
+    ));
+}
+void SmartMap::commitTile(LocatedTile located_tile)
+{
+    if (!history_stack){
+        setUntrackedTile(located_tile);
+        return;
+    }
+    history_stack->quickCommit(located_tile);
+}
+
 void SmartMap::addTile(int x, int y, Tile tile){
     if (!history_stack){
         setUntrackedTile(x, y, tile);
         return;
     }
     history_stack->trackChange({tile, x, y});
+}
+
+void SmartMap::addTileID(int x, int y, uint16_t tile_id){
+    if (!history_stack){
+        setUntrackedTileID(x, y, tile_id);
+        return;
+    }
+    history_stack->trackChange(LocatedTile{
+        tile_id,
+        matrix[y][x].entityID,
+        x,
+        y
+    });
+}
+
+void SmartMap::addEntityID(int x, int y, uint16_t entity_id){
+    if (!history_stack){
+        setUntrackedEntityID(x, y, entity_id);
+        return;
+    }
+    history_stack->trackChange({
+        matrix[y][x].tileID,
+        entity_id,
+        x,
+        y
+    });
+}
+
+void SmartMap::addTile(LocatedTile located_tile){
+    if (!history_stack){
+        setUntrackedTile(located_tile);
+        return;
+    }
+    history_stack->trackChange(located_tile);
 }
 
 DirectionalLine SmartMap::getLineFacingEdge(const Edge &edge, const d2kmapapi::Direction &facingDirection){
