ac8086306ba290d1191a080f3d6d1753615d2a1b
he1kor <helkor.rightway@gmail.com>
2025-02-09 16:39:31 +0300

    Feat: AbstractHistory & ChangeTracker implementing Strategy pattern

---
+++ include/change_tracker.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template<typename Change>
+class ChangeTracker{
+    public:
+        virtual Change getOldState(const Change& changing_state) const = 0;
+        virtual void applyChange(Change change) = 0;
+};
