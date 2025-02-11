e180c69746a227e021cfdfa30b94a8a5ad196145
he1kor <helkor.rightway@gmail.com>
2025-02-11 23:59:20 +0300

    fix: removed cpp defining

--- include/priority_map.h
+++ include/priority_map.h
@@ -3,17 +3,36 @@
 
 template <typename T>
 class PriorityMap{
-public:
-    PriorityMap();
-    PriorityMap(bool is_strict);
-    PriorityMap(std::map<T, int> priority_map);
-    PriorityMap(std::map<T, int> priority_map, bool is_strict);
-    void addPriority(T value, int priority);
-    int getPriority(T value);
+public:PriorityMap(){}
 
-    bool isHigher(T old_value, T new_value);
-    void setStrict(bool is_strict);
-    bool isStrict();
+    PriorityMap(bool is_strict) : is_strict{is_strict}{}
+
+    PriorityMap(std::map<T, int> priority_map) : priority_map{priority_map}{}
+
+    PriorityMap(std::map<T, int> priority_map, bool is_strict) : priority_map{priority_map}, is_strict{is_strict}{}
+
+    void addPriority(T value, int priority){
+        priority_map[value] = priority;
+    }
+
+    int getPriority(T value){
+        return priority_map.at[value];
+    }
+
+    bool isHigher(T old_value, T new_value){
+        if (is_strict)
+            return priority_map.at[new_value] > priority_map.at[old_value];
+        return priority_map.at[new_value] >= priority_map.at[old_value];
+    }
+
+    void setStrict(bool is_strict){
+        this->is_strict = is_strict;
+    }
+
+    bool isStrict(){
+        return is_strict;
+    }
+
 private:
     std::map<T, int> priority_map;
     bool is_strict = false;
