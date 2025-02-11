c682a486481f7200ff4e16c1324068558ea8bb97
he1kor <helkor.rightway@gmail.com>
2025-02-11 23:59:52 +0300

    feat: add pattern, numbering, constructor, fixed cpp defining

--- include/wall.h
+++ include/wall.h
@@ -3,16 +3,25 @@
 #include <vector>
 #include "priority_map.h"
 
+static const int NO_NUMBER = -1;
+
 template <typename T>
 class Wall{
-    public:
-        Wall();
-        Wall(int width, int height);
-        
-
     private:
         int width = 0;
         int height = 0;
         PriorityMap<T> priority_map;
+        std::vector<std::vector<T>> pattern;
+
+        std::vector<std::vector<int>> numbering;
         std::vector<std::vector<T>> segments;
+    public:
+        Wall(int width, int height, std::vector<std::vector<T>> pattern, PriorityMap<T> priority_map) :
+            width(width),
+            height(height),
+            pattern{pattern},
+            numbering{std::vector(height, std::vector(width, NO_NUMBER))},
+            segments{std::vector(height, std::vector<T>(height))},
+            priority_map{priority_map}
+            {}
 };
