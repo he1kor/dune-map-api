88ba39c8ea3834acc050aba7de73623158e1b043
he1kor <helkor.rightway@gmail.com>
2025-02-16 22:14:19 +0300

    fix: priority map CE

--- include/priority_map.h
+++ include/priority_map.h
@@ -16,13 +16,13 @@ public:PriorityMap(){}
     }
 
     int getPriority(T value){
-        return priority_map.at[value];
+        return priority_map.at(value);
     }
 
     bool isHigher(T new_value, T old_value){
         if (is_strict)
-            return priority_map.at[new_value] > priority_map.at[old_value];
-        return priority_map.at[new_value] >= priority_map.at[old_value];
+            return priority_map.at(new_value) > priority_map.at(old_value);
+        return priority_map.at(new_value) >= priority_map.at(old_value);
     }
 
     void setStrict(bool is_strict){
